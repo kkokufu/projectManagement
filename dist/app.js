b8aa80893829243a39b380eaa8a9d8201e99af52
Keita Kokufu <>
2023-06-09 15:57:07 +0900

    submitイベントによるプロジェクト入力値所得

--- dist/app.js
+++ dist/app.js
@@ -5,10 +5,24 @@ class ProjectInput {
         this.mainElement = document.getElementById("app");
         const inputElement = document.importNode(this.templeteElement.content, true);
         this.element = inputElement.firstElementChild;
+        this.title = document.getElementById("title");
+        this.description = document.getElementById("description");
+        this.manday = document.getElementById("manday");
         this.attach();
+        this.submitHandler();
     }
     attach() {
         this.mainElement.insertAdjacentElement('afterbegin', this.element);
+    }
+    getInputInfo(event) {
+        event.preventDefault();
+        const titleValue = this.title.value;
+        const descriptionValue = this.description.value;
+        const mandayValue = this.manday.value;
+        console.log(titleValue, descriptionValue, mandayValue);
+    }
+    submitHandler() {
+        this.element.addEventListener("submit", this.getInputInfo);
     }
 }
 const input = new ProjectInput();
--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ class ProjectInput {
     templeteElement: HTMLTemplateElement;
     mainElement: HTMLDivElement;
     element: HTMLFormElement;
+    title: HTMLInputElement;
+    description: HTMLTextAreaElement;
+    manday: HTMLInputElement;
 
     constructor() {
         this.templeteElement = document.getElementById("project-input")! as HTMLTemplateElement;
@@ -9,11 +12,29 @@ class ProjectInput {
         const inputElement = document.importNode(this.templeteElement.content, true)
         this.element = inputElement.firstElementChild! as HTMLFormElement;
 
+        this.title = document.getElementById("title")! as HTMLInputElement;
+        this.description = document.getElementById("description")! as HTMLTextAreaElement;
+        this.manday = document.getElementById("manday")! as HTMLInputElement;
+
+
         this.attach();
+        this.submitHandler()
     }
 
     private attach() {
         this.mainElement.insertAdjacentElement('afterbegin', this.element);
+    }
+
+    private getInputInfo(event:Event) {
+        event.preventDefault();
+        const titleValue = this.title.value;
+        const descriptionValue = this.description.value;
+        const mandayValue = this.manday.value;
+        console.log(titleValue, descriptionValue, mandayValue);
+    }
+
+    private submitHandler() {
+        this.element.addEventListener("submit", this.getInputInfo);
     }
 }
 
